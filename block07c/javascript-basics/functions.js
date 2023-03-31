7efeb38b2303d94c20bb4c7fcbb9126bb7892a39
BigMosey <willy.salas6996@gmail.com>
2023-03-30 19:34:14 -0700

    Finished challenge

---
+++ block07c/javascript-basics/functions.js
@@ -0,0 +1,14 @@
+function greeting(name){
+    return `Hello,${name}!`
+}
+function cookSomethingFried(foods){
+    const friedFoods =`fried ${foods}`
+    return `Here is your ${friedFoods}`
+}
+
+function sub(a,b){
+    return  a -b;
+}
+
+console.log(greeting("Willy"))
+console.log(cookSomethingFried("banana"))
---
+++ block07c/javascript-basics/javascript-object.js
@@ -0,0 +1,15 @@
+const object = {
+    key :"value"
+}
+// nested objected
+const willyProfile ={
+    firstName:"Willy",
+    lastName:"Salas",
+    hobby:"working out",
+    favoriteFood:"ceviche",
+    age:"24"
+}
+
+console.log("willy likes to be lazy")
+console.log(`${willyProfile.firstName} likes to be ${hobby}`)
+console.log(`Mr. ${willyProfile.lastName} likes to eat ${favoriteFood}`)
